b286b9c6d7767208420246ca8b47bb206cdee359
Nour Al Safadi <nour.safadi7@icloud.com>
2025-09-10 16:45:39 +0200

    corrects table alising

--- models/staging/staging_airports.sql
+++ models/staging/staging_airports.sql
@@ -2,8 +2,8 @@ WITH
 joined_table AS (
     SELECT 
         * 
-    FROM {{source('flights_data', 'airports')}} airports a
-    JOIN {{source('flights_data', 'regions')}} regions r
+    FROM {{ source('flights_data', 'airports') }}  a
+    JOIN {{ source('flights_data', 'regions') }}  r
     ON a.country = r.country
 )
 SELECT * FROM joined_table
