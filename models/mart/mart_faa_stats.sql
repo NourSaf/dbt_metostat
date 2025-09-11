9096a1303006c2055383bf6604802088037d0639
Nour Al Safadi <nour.safadi7@icloud.com>
2025-09-11 14:49:16 +0200

    removes query from mart total stats

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,22 +1,12 @@
 -- unique number of departures connections
-
 -- unique number of arrival connections
-
 -- how many flight were planned in total (departures & arrivals)
-
 -- how many flights were canceled in total (departures & arrivals)
-
 -- how many flights were diverted in total (departures & arrivals)
-
 -- how many flights actually occured in total (departures & arrivals)
-
 -- (optional) how many unique airplanes travelled on average
-
 -- (optional) how many unique airlines were in service on average
-
 -- add city, country and name of the airport
-
-SELECT * FROM prep_flights WHERE dep_time IS NULL;
 
 WITH 
 dep_flights AS (
