2118a7025ccee98f9fa71e3d2bf6d22815bcee95
Nour Al Safadi <nour.safadi7@icloud.com>
2025-09-11 23:23:37 +0200

    adds additional metrices to mart daily flights and weather stats

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -3,7 +3,10 @@ flights_dep AS (
     SELECT
         p.flight_date,
         p.origin AS airport_code,
-        COUNT(DISTINCT p.dest) AS total_flights_dep
+        COUNT(DISTINCT p.dest) AS total_flights_dep,
+        COUNT(sched_dep_time) AS planned_dep,
+        SUM(cancelled) AS cancelled_dep,
+        SUM(diverted) AS total_diverted_dep
     FROM prep_flights p
     WHERE p.origin IN ('JFK', 'LAX', 'MIA')
     GROUP BY p.flight_date, p.origin
@@ -12,17 +15,23 @@ flights_arr AS (
     SELECT
         p.flight_date,
         p.dest AS airport_code,
-        COUNT(DISTINCT p.origin) AS total_flights_arr
+        COUNT(DISTINCT p.origin) AS total_flights_arr,
+        COUNT(sched_arr_time) AS planned_arr,
+        SUM(cancelled) AS cancelled_arr,
+        SUM(diverted) AS total_diverted_arr
     FROM prep_flights p
     WHERE p.dest IN ('JFK', 'LAX', 'MIA')
     GROUP BY p.flight_date, p.dest
 )
-SELECT *
-FROM flights_dep 
-JOIN flights_arr
+SELECT
+    dp.airport_code,
+    dp.flight_date,
+    dp.total_flights_dep,
+    ar.total_flights_arr,
+    (dp.planned_dep + ar.planned_arr) AS total_planned,
+    (dp.cancelled_dep + cancelled_arr) AS total_cancelled,
+    (total_diverted_dep + total_diverted_arr) AS total_diverted
+FROM flights_dep dp
+JOIN flights_arr ar
 USING (flight_date, airport_code)
 ORDER BY total_flights_dep
-
--- for the selected airports total number of departures 
-
-SELECT * FROM prep_flights;
